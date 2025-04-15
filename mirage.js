7a604787756e25b17cddbfc79c9cfb98bc2dcae9
kritika <kritikabhardwaj2513@gmail.com>
2025-04-15 14:35:46 +0530

    typintest

---
+++ mirage.js
@@ -0,0 +1,16 @@
+// // mirage.js
+// import { createServer } from "https://unpkg.com/miragejs@latest?module";
+
+// export function makeServer() {
+//   createServer({
+//     routes() {
+//       this.namespace = "api"; // Our fake API will be at /api/quote
+
+//       this.get("/quote", () => {
+//         return {
+//           content: "Mirage JS lets you mock APIs for frontend development!"
+//         };
+//       });
+//     }
+//   });
+// }
